45587b4d2637f9ec1c237ed36b2aaaac6607b7d9
athultpme <137172798+athultpme@users.noreply.github.com>
2025-11-10 23:29:48 +0530

    Update pymodbus.py

--- pymodbus.py
+++ pymodbus.py
@@ -1,13 +1,10 @@
-from pymodbus3.client.sync import ModbusTcpClient
-
-inp = input(u"Press any key and enter to send a packet... (Just enter to quit)")
+from pymodbus.client.sync import ModbusTcpClient
 
 client = ModbusTcpClient('100.100.100.3')
-
-while (inp):
-     client.write_coil(1, True)
-     result = client.read_coils(1, 1)
-     print(result.bits[0])
-     client.close()
-    
-     inp = input(u"Press any key and enter to send a packet... (Just enter to quit) ")
+inp = input("Press any key and enter to send a packet... (Just enter to quit)")
+while inp:
+    client.write_coil(1, True)
+    result = client.read_coils(1, 1)
+    print(result.bits[0])
+    inp = input("Press any key and enter to send a packet... (Just enter to quit)")
+client.close()
